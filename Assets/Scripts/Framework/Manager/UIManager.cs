37344dd62afe9e79e1d0998f735b48b3f63581bb
wzh <shilofel>
2026-02-23 01:02:39 +0800

    UI分组，从而按group分级

--- Assets/Scripts/Framework/Manager/UIManager.cs
+++ Assets/Scripts/Framework/Manager/UIManager.cs
@@ -7,7 +7,34 @@ public class UIManager : MonoBehaviour
     //缓存UI
     Dictionary<string, GameObject> m_UI = new Dictionary<string, GameObject>();
 
-    public void OpenUI(string uiName, string luaName)
+    //ui分组
+    Dictionary<string, Transform> m_UIGroups = new Dictionary<string, Transform>();
+
+    private Transform m_UIParent;
+
+    private void Awake()
+    {
+        m_UIParent = this.transform.parent.Find("UI");
+    }
+    //热更
+    public void SetUIGroup(List<string> group)
+    {
+        for (int i = 0; i < group.Count; i++)
+        {
+            GameObject go = new GameObject("Group-" + group[i]);
+            go.transform.SetParent(m_UIParent, false);
+            m_UIGroups.Add(group[i], go.transform);
+        }
+    }
+
+    Transform GetUIGroup(string group)
+    {
+        if (!m_UIGroups.ContainsKey(group))
+            Debug.LogError("group is not exist");
+        return m_UIGroups[group];
+    }
+
+    public void OpenUI(string uiName,string group, string luaName)
     {
         GameObject ui = null;
         if(m_UI.TryGetValue(uiName,out ui))
@@ -21,6 +48,9 @@ public class UIManager : MonoBehaviour
         {
             ui = Instantiate(obj) as GameObject;
             m_UI.Add(uiName, ui);
+
+            Transform parent = GetUIGroup(group);
+            ui.transform.SetParent(parent, false);
             UILogic uiLogic = ui.AddComponent<UILogic>();
             uiLogic.Init(luaName);
             uiLogic.OnOpen();
